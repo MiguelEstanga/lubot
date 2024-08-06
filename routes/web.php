62dc0f22d0f44dc140ff4dfb744a74755e9271e9
MiguelEstanga12 <miguelestanga12@gmail.com>
2024-08-06 12:02:52 -0400

    lubot

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,15 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Http;
-Route::get('/api/autowebs_clientes', function () {
-    return DB::table('autowebs_clientes')->get();
+
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/api/autowebs_negocios', function () {
-    return DB::table('autowebs_negocios')->get();
-});
-Route::get('/api/testBot', function () {
-      // Ruta completa al ejecutable de Python
-      $workingDirectory = 'C:/Users/user/Desktop/lubot_bot/lubot';
-        
-      // Ruta completa al ejecutable de Python
-      $pythonPath = 'C:/Users/user/AppData/Local/Programs/Python/Python313/python.exe';
-      
-      // Nombre del script de Python
-      $scriptName = 'whatsapp.py';
-      // Construir el comando
-      $command = "cd $workingDirectory && $pythonPath $scriptName";
-      // Ejecutar el comando y capturar la salida
-       $output = shell_exec($command . ' 2>&1'); // Re
-      return response()->json([
-        'output' => $output,
-        'mensage' => 'ejecutando bot'
-      ]);
-});
-
+// este controlador activa el bot 
 Route::get('/api/activar_bot', function () {
     //return 'aqui vamos';
     $response = Http::withHeaders(['Accept' => 'application/json'])
@@ -37,6 +17,16 @@ Route::get('/api/activar_bot', function () {
     $clientes = $response->body();
 });
 
+//este controloador returno los clientes 
+Route::get('/api/autowebs_clientes', function () {
+    return DB::table('autowebs_clientes')->get();
+});
+
+Route::get('/api/autowebs_negocios', function () {
+    return DB::table('autowebs_negocios')->get();
+});
+
+//returna el negocio que a pasado las condiciones 
 Route::get('/api/autowebs_negocios/{numero}', function ($numero) {
     $check =  DB::table('autowebs_negocios')->where('telefono' , $numero);
 
@@ -44,12 +34,12 @@ Route::get('/api/autowebs_negocios/{numero}', function ($numero) {
 
     $empresa = $check->first();
     $data = array();
-   
+
     $pais = DB::table('paises')->where('id' , $empresa->pais_id )->first();
     $ciudad = DB::table('ciudades')->where('id' , $empresa->ciudad_id )->first();
     $barrio = DB::table('barrios')->where('id' , $empresa->barrio_id )->first();
     $negocio = DB::table('tipos_negocio')->where('id' , $empresa->tipo_negocio_id )->first();
-   
+
     $data['pais'] = $pais->nombre ?? null;
     $data['ciudad'] = $ciudad->nombre ?? null;
     $data['barrio'] = $barrio->nombre ?? null;
@@ -59,22 +49,49 @@ Route::get('/api/autowebs_negocios/{numero}', function ($numero) {
     return $data;
 });
 
+//test de prueba para probar la logica de los clientes que pasan el filtro 
 Route::get('api/test' , function(){
     $clientes = DB::table('autowebs_clientes')->get();
+
     $clientes_autorizado = [];
-    foreach ($clientes as $cliente) 
+
+    foreach ($clientes as $cliente)
     {
         if($cliente->ws === 1 && $cliente->bot_finalizado === 1 && $cliente->valid === 1 )
         {
             $check =  DB::table('autowebs_negocios')->where('telefono' , $cliente->telefono);
             if($check->exists()) $clientes_autorizado[] = $check->first();
         }
-        
-    }
+      }
 
     return $clientes_autorizado;
 });
 
+//returna los paises de lubot master
+Route::get('/api/paises' , function () {
+    $paises = DB::table('paises')->get();
+    return $paises;
+});
+
+//returna los barrios de lubot master
+Route::get('/api/barrios' , function () {
+    $paises = DB::table('barrios')->get();
+    return $paises;
+});
+
+//returna los negocion de lubot master
+Route::get('/api/tipos_negocios' , function () {
+    $paises = DB::table('tipos_negocio')->get();
+    return $paises;
+});
+
+//returna las ciudades de lubot master
+Route::get('/api/ciudades' , function () {
+    $paises = DB::table('ciudades')->get();
+    return $paises;
+});
+
+// rutas controladores para el login 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
