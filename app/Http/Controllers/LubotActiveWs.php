54cf5329829697f216cf5c71867e662eee9656fe
MiguelEstanga12 <miguelestanga12@gmail.com>
2024-08-07 10:38:53 -0400

    modificacion de so

--- app/Http/Controllers/LubotActiveWs.php
+++ app/Http/Controllers/LubotActiveWs.php
@@ -21,15 +21,23 @@ class LubotActiveWs extends Controller
 
     function iniciar_sesion_whatsapp_ws($company_id)
     { 
-        $workingDirectory = env('RUTA_ARCHIVO_PY'); // ruta por defecto del 
-        // Ruta al ejecutable de Python y al script
-        $pythonPath = env('PYTHON_PATH');// ejcutor
-        $scriptPath = $workingDirectory.'\iniciar_sesion.py'; 
-        $command = "start powershell.exe -NoExit -Command \"cd '$workingDirectory'; & '$pythonPath' '$scriptPath' --company_id $company_id --type ws\"";
+        $workingDirectory = env('RUTA_ARCHIVO_PY'); // Ruta por defecto del archivo
+        $pythonPath = env('PYTHON_PATH'); // Ejecutor
+        $scriptPath = $workingDirectory . '/iniciar_sesion.py'; 
+    
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            // Comando para Windows
+            $command = "start powershell.exe -NoExit -Command \"cd '$workingDirectory'; & '$pythonPath' '$scriptPath' ";
+        } else {
+            // Comando para Unix/Linux
+            $command = "gnome-terminal -- bash -c \"cd '$workingDirectory'; '$pythonPath' '$scriptPath' --company_id $company_id --type ws; exec bash\"";
+        }
+    
         // Ejecutar el comando en segundo plano
         pclose(popen($command, 'r'));
+    
         // Opcionalmente, puedes devolver una respuesta al navegador
-        echo "PowerShell iniciado y ejecutando el script de Python en $workingDirectory.";
+        echo "Terminal iniciada y ejecutando el script de Python en $workingDirectory.";
         return json_encode(['response' => 1]);
     }
    
