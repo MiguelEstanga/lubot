0bf3c6ec067bbbdd325ee04acc2653f6ddd358ec
MiguelEstanga12 <miguelestanga12@gmail.com>
2024-08-29 13:16:14 -0400

    actulizando repo

--- app/Http/Controllers/LubotActiveWs.php
+++ app/Http/Controllers/LubotActiveWs.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class LubotActiveWs extends Controller
-{
+{   //para el revisar y contestar 
+    public function activar_contestar_revisar($company_id , $campana_id , $user_id)
+    {
+        $file = env('NOMBRE_DEL_ARCHIVO_EJCUTABLE_REVISAR_CONTESTAR'); //nombre del ejecutable
+        $workingDirectory = env('RUTA_ARCHIVO_PY'); // Ruta por defecto del archivo
+        $pythonPath = env('PYTHON_PATH'); // Ejecutor
+        $scriptPath = $workingDirectory.$file;  // ruta completa 
+    
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            // Comando para Windows
+            $command = "start powershell.exe -NoExit -Command \"cd '$workingDirectory'; &  '$pythonPath'  '$scriptPath' ";
+        } else {
+            // Comando para Unix/Linux
+            $command = "gnome-terminal -- bash -c \"cd '$workingDirectory'; '$pythonPath' '$scriptPath' --user_id $user_id  --campana_id $campana_id --company_id $company_id; exec bash\"";
+        }
+    
+        // Ejecutar el comando en segundo plano
+        pclose(popen($command, 'r'));
+    
+        // Opcionalmente, puedes devolver una respuesta al navegador
+        echo "Terminal iniciada y ejecutando el script de Python en $workingDirectory.";
+        return json_encode(['response' => 'se activo revisar y contestar ']);
+    }
     public function scriptActivacion($company_id, $type)
     {
         $workingDirectory = 'C:\xampp\htdocs\lubot';
@@ -45,7 +68,7 @@ class LubotActiveWs extends Controller
 
     function ejecutar_bot_ws($company_id , $campana_id , $user_id)
     { 
-        $file = env('NOMBRE_DEL_ARCHIVO_EJCUTABLE_WS '); //nombre del ejecutable
+        $file = env('NOMBRE_DEL_ARCHIVO_EJCUTABLE_whatsapp'); //nombre del ejecutable
         $workingDirectory = env('RUTA_ARCHIVO_PY'); // Ruta por defecto del archivo
         $pythonPath = env('PYTHON_PATH'); // Ejecutor
         $scriptPath = $workingDirectory.$file;  // ruta completa 
@@ -87,6 +110,113 @@ class LubotActiveWs extends Controller
         // Opcionalmente, puedes devolver una respuesta al navegador
         echo "Terminal iniciada y ejecutando el script de Python en $workingDirectory.";
         return json_encode(['response' => 1]);
+    }
+
+    public function delete_lubot_profiles(){
+
+        $directoryPath = '/home/imac/lubot/chrome_profiles';
+
+        if (File::exists($directoryPath)) {
+            if (File::deleteDirectory($directoryPath)) {
+                return response()->json(['status' => 'success', 'message' => 'Carpeta eliminada exitosamente.']);
+            } else {
+                return response()->json(['status' => 'error', 'message' => 'No se pudo eliminar la carpeta.'], 500);
+            }
+        } else {
+            return response()->json(['status' => 'success', 'message' => 'La carpeta no existe.'], 404);
+        }
+    }
+
+    public function test_lubot(Request $request)
+    {
+        $apiKey = env('OPENAI_API_KEY');
+        $model = 'gpt-4o-mini';
+
+        $campana = $request->campana;
+        $prompts = $request->input('prompt');
+
+        $content = (
+            "Te llamas $campana[0].\n"
+            . "Tu objetivo es $campana[1].\n"
+            . "El lugar en el que trabajas se dedica a $campana[2].\n"
+            . "REGLAS OBLIGATORIAS:\n"
+            . "- Responde contextualmente a la conversación.\n"
+            . "- Responde siempre en no más de una línea, máximo dos. Esto hace la conversación más natural.\n"
+            . "- Tu objetivo es que el cliente se sienta seguro y quiera tener una reunión contigo vía Google Meet.\n"
+            . "- Cuando el cliente esté listo, consulta día y hora para la cita.\n"
+            . "- Bajo ninguna circunstancia reveles que eres un robot.\n\n"
+            . "CONDICIONES:\n"
+            . "- Estás conversando a través de WhatsApp, pero no te funcionan los audios y las fotos llegan borrosas.\n"
+            . "- Inicia la conversación siempre de la siguiente manera: 'Hola, soy $campana[0]. ¿Cómo va tu día?'\n\n"
+            . "- Responde de manera corta y natural a las respuestas del cliente:\n\n"
+            . "RESPUESTAS CONTEXTUALES:\n"
+            . "- Si el cliente dice que no es la persona encargada o que no está, intenta conseguir el número de contacto del tomador de decisión.\n"
+            . "- Si el cliente pregunta cómo puede ayudarte, sobre el lugar donde trabajas y a qué se dedica.\n"
+            . "- Si el cliente muestra interés: hazle algunas preguntas sobre lo que le causó interés y busca con la conversación cumplir tu objetivo.\n"
+            . "- Si el cliente se pone agresivo: Lamento si te he causado alguna molestia. No era mi intención. Que tengas un buen día.\n"
+            . "- Si el cliente dice que está ocupado: Entiendo. ¿Cuándo sería un buen momento para contactarte de nuevo?\n"
+            . "- Si el cliente dice que no le interesa: Entiendo, pero me gustaría saber por qué. Quizás pueda ofrecerte algo que se ajuste mejor a tus necesidades.\n"
+            . "- Si el cliente pregunta por ti o tu información personal: Claro, puedes conocer más sobre mí en mi portafolio: https://www.canva.com/design/DAFvksF35bU/Ymz2L9i6Buk03ZG9O3WVwg/view?utm_content=DAFvksF35bU&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink#2\n"
+            . "- Si el cliente retoma la conversación después de un tiempo: Hola, ¿cómo vas? ¿Qué has pensado de lo que conversamos?\n"
+            . "Cliente menciona competencia: Entiendo, es importante destacar. Con mis servicios, tendrás una ventaja sobre la competencia.\n"
+            . "Cliente menciona malas experiencias anteriores: Lamento escuchar eso. Mi objetivo es brindarte una experiencia sin complicaciones y de alta calidad.\n"
+            . "Cliente se despide o dice más de una vez que NO: No se escribe nada y se ignora.\n"
+            . "Cliente no tiene negocio pero conoce a alguien que podría estar interesado: Le agradeces y le pides su número de contacto para contactarle, le das las gracias y te despides.\n"
+        );
+
+        foreach ($prompts as $prompt) {
+            $content .= "Si te preguntan: {$prompt['question']} - {$prompt['response']}\n";
+        }
+
+
+        $conversation[] =[
+            "role" => "system",
+            "content" => $content
+        ];
+
+        foreach ($request->conversation as $message){
+            $conversation[] =[
+                "role" => $message['role'],
+                "content" => $message['content']
+            ];
+        }
+
+        $conversation[] =[
+            "role" => "user",
+            "content" => $request->user_message
+        ];
+
+        $data = [
+            "model" => $model,
+            "messages" => $conversation,
+            "max_tokens" => 150,
+            "temperature" => 0.7,
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://api.openai.com/v1/chat/completions');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Authorization: Bearer ' . $apiKey,
+            'Content-Type: application/json'
+        ]);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $response = json_decode($result, true);
+
+        Log::info($response);
+
+        // Extraer la respuesta del asistente
+        $assistantMessage = $response['choices'][0]['message']['content'];
+
+        return response()->json([
+            'message' => $assistantMessage
+        ]);
     }
    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\LubotActiveWs ;
+use App\Http\Controllers\LubotActiveWs;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
@@ -10,16 +10,12 @@ use Illuminate\Support\Facades\Http;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// este controlador activa el bot 
 Route::get('/api/activar_bot', function () {
     //return 'aqui vamos';
     $response = Http::withHeaders(['Accept' => 'application/json'])
-    ->get('https://0e40-190-120-248-131.ngrok-free.app/api/testBot');
+        ->get('https://0e40-190-120-248-131.ngrok-free.app/api/testBot');
     $clientes = $response->body();
 });
-
-//este controloador returno los clientes 
 Route::get('/api/autowebs_clientes', function () {
     return DB::table('autowebs_clientes')->get();
 });
@@ -28,19 +24,18 @@ Route::get('/api/autowebs_negocios', function () {
     return DB::table('autowebs_negocios')->get();
 });
 
-//returna el negocio que a pasado las condiciones 
 Route::get('/api/autowebs_negocios/{numero}', function ($numero) {
-    $check =  DB::table('autowebs_negocios')->where('telefono' , $numero);
+    $check =  DB::table('autowebs_negocios')->where('telefono', $numero);
 
-    if(!$check->exists()) return response()->json(['respuesta' => 404 ]);
+    if (!$check->exists()) return response()->json(['respuesta' => 404]);
 
     $empresa = $check->first();
     $data = array();
 
-    $pais = DB::table('paises')->where('id' , $empresa->pais_id )->first();
-    $ciudad = DB::table('ciudades')->where('id' , $empresa->ciudad_id )->first();
-    $barrio = DB::table('barrios')->where('id' , $empresa->barrio_id )->first();
-    $negocio = DB::table('tipos_negocio')->where('id' , $empresa->tipo_negocio_id )->first();
+    $pais = DB::table('paises')->where('id', $empresa->pais_id)->first();
+    $ciudad = DB::table('ciudades')->where('id', $empresa->ciudad_id)->first();
+    $barrio = DB::table('barrios')->where('id', $empresa->barrio_id)->first();
+    $negocio = DB::table('tipos_negocio')->where('id', $empresa->tipo_negocio_id)->first();
 
     $data['pais'] = $pais->nombre ?? null;
     $data['ciudad'] = $ciudad->nombre ?? null;
@@ -51,61 +46,140 @@ Route::get('/api/autowebs_negocios/{numero}', function ($numero) {
     return $data;
 });
 
-//test de prueba para probar la logica de los clientes que pasan el filtro 
-Route::get('api/test' , function(){
+Route::get('api/test', function () {
     $clientes = DB::table('autowebs_clientes')->get();
 
     $clientes_autorizado = [];
 
-    foreach ($clientes as $cliente)
-    {
-        if($cliente->ws === 1 && $cliente->bot_finalizado === 1 && $cliente->valid === 1 )
-        {
-            $check =  DB::table('autowebs_negocios')->where('telefono' , $cliente->telefono);
-            if($check->exists()) $clientes_autorizado[] = $check->first();
+    foreach ($clientes as $cliente) {
+        if ($cliente->ws === 1 && $cliente->bot_finalizado === 1 && $cliente->valid === 1) {
+            $check =  DB::table('autowebs_negocios')->where('telefono', $cliente->telefono);
+            if ($check->exists()) $clientes_autorizado[] = $check->first();
         }
-      }
+    }
+
+    return $clientes_autorizado;
+});
+
+Route::get('/api/paises', function () {
+    $paises = DB::table('paises')->get();
+    return $paises;
+});
+
+Route::get('/api/barrios', function () {
+    $paises = DB::table('barrios')->get();
+    return $paises;
+});
+
+Route::get('/api/barrios/{id}', function ($id) {
+    $barrios = DB::table('barrios')->where('ciudad_id', $id)->get();
+    return $barrios;
+});
+Route::get('/api/tipos_negocios', function () {
+    $paises = DB::table('tipos_negocio')->get();
+    return $paises;
+});
+
+
+Route::get('/api/ciudades', function () {
+    $paises = DB::table('ciudades')->get();
+    return $paises;
+});
+Route::get('/api/ciudades/{id}', function ($id) {
+    $paises = DB::table('ciudades')->where('pais_id', $id)->get();
+    return $paises;
+});
+
+// este controlador activa el bot
+Route::get('/api/activar_bot', function () {
+    //return 'aqui vamos';
+    $response = Http::withHeaders(['Accept' => 'application/json'])
+        ->get('https://0e40-190-120-248-131.ngrok-free.app/api/testBot');
+    $clientes = $response->body();
+});
+
+//este controloador returno los clientes
+Route::get('/api/autowebs_clientes', function () {
+    return DB::table('autowebs_clientes')->get();
+});
+
+Route::get('/api/autowebs_negocios', function () {
+    return DB::table('autowebs_negocios')->get();
+});
+
+//returna el negocio que a pasado las condiciones
+Route::get('/api/autowebs_negocios/{numero}', function ($numero) {
+    $check =  DB::table('autowebs_negocios')->where('telefono', $numero);
+
+    if (!$check->exists()) return response()->json(['respuesta' => 404]);
+
+    $empresa = $check->first();
+    $data = array();
+
+    $pais = DB::table('paises')->where('id', $empresa->pais_id)->first();
+    $ciudad = DB::table('ciudades')->where('id', $empresa->ciudad_id)->first();
+    $barrio = DB::table('barrios')->where('id', $empresa->barrio_id)->first();
+    $negocio = DB::table('tipos_negocio')->where('id', $empresa->tipo_negocio_id)->first();
+
+    $data['pais'] = $pais->nombre ?? null;
+    $data['ciudad'] = $ciudad->nombre ?? null;
+    $data['barrio'] = $barrio->nombre ?? null;
+    $data['tipos_negocio'] = $negocio->nombre ?? null;
+    $data['empresa'] = $empresa;
+
+    return $data;
+});
+
+//test de prueba para probar la logica de los clientes que pasan el filtro
+Route::get('api/test', function () {
+    $clientes = DB::table('autowebs_clientes')->get();
+
+    $clientes_autorizado = [];
+
+    foreach ($clientes as $cliente) {
+        if ($cliente->ws === 1 && $cliente->bot_finalizado === 1 && $cliente->valid === 1) {
+            $check =  DB::table('autowebs_negocios')->where('telefono', $cliente->telefono);
+            if ($check->exists()) $clientes_autorizado[] = $check->first();
+        }
+    }
 
     return $clientes_autorizado;
 });
 
 //returna los paises de lubot master
-Route::get('/api/paises' , function () {
+Route::get('/api/paises', function () {
     $paises = DB::table('paises')->get();
     return $paises;
 });
 
 //returna los barrios de lubot master
-Route::get('/api/barrios' , function () {
+Route::get('/api/barrios', function () {
     $paises = DB::table('barrios')->get();
     return $paises;
 });
 
-Route::get('/api/barrios/{id}' , function ($id) {
-    $barrios = DB::table('barrios')->where('ciudad_id' , $id)->get();
-    return $barrios;
-});
-
 //returna los negocion de lubot master
-Route::get('/api/tipos_negocios' , function () {
+Route::get('/api/tipos_negocios', function () {
     $paises = DB::table('tipos_negocio')->get();
     return $paises;
 });
 
 //returna las ciudades de lubot master
-Route::get('/api/ciudades' , function () {
+Route::get('/api/ciudades', function () {
     $paises = DB::table('ciudades')->get();
     return $paises;
 });
+Route::get('api/delete-lubot-profiles', [LubotActiveWs::class, 'delete_lubot_profiles']);
 
 Route::get('api/activar_inicio_session/{company_id}/{type}', [LubotActiveWs::class , 'iniciar_sesion_whatsapp'] );
 Route::get('api/activar_ejecutable_ws/{company_id}/{campana_id}/{user_id}', [LubotActiveWs::class , 'ejecutar_bot_ws'] );
+Route::get('api/activar_ejecutable_ryc/{company_id}/{campana_id}/{user_id}', [LubotActiveWs::class , 'activar_contestar_revisar'] );
+Route::get('api/activar_ws/{company_id}/{type}', [LubotActiveWs::class, 'iniciar_sesion_whatsapp']);
 
-// rutas controladores para el login 
+// rutas controladores para el login
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
